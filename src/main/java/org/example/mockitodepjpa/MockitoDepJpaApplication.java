cb6723e91154e5499c3cb10079381e8379868d2a
Mateus Ribeiro <MatZeraH64@hotmail.com>
2026-04-26 22:30:45 -0300

    Main do projeto Spring

---
+++ src/main/java/org/example/mockitodepjpa/MockitoDepJpaApplication.java
@@ -0,0 +1,14 @@
+package org.example.mockitodepjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoDepJpaApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoDepJpaApplication.class, args);
+		System.out.println("Aplicação iniciada com sucesso!");
+	}
+	
+}
