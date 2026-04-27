26e2c4d768a9246b1849f096616f11a3833f3e0b
Mateus Ribeiro <MatZeraH64@hotmail.com>
2026-04-26 22:31:06 -0300

    Teste gerado automaticamente pelo Spring

---
+++ src/test/java/org/example/mockitodepjpa/MockitoDepJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mockitodepjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoDepJpaApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
